0b2d14815534455c26b67f05c7fc7f2d01d915a0
Ian <ianmaciel76@gmail.com>
2022-05-23 23:31:22 -0300

    alterando groupId e adicionando requestMapping

--- student-service/src/main/java/com/github/ianmaciel7/schoolmicroservice/studentservice/StudentServiceApplication.java
+++ student-service/src/main/java/com/github/ianmaciel7/schoolmicroservice/studentservice/StudentServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
@@ -15,6 +16,7 @@ public class StudentServiceApplication {
 	}
 
 	@RestController
+	@RequestMapping("/example")
 	class ExampleController {
 
 		@GetMapping("/test")
