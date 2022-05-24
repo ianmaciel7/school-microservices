187ad11a99dac573d01886f7cef7d4a3200a6015
Ian <ianmaciel76@gmail.com>
2022-05-23 23:31:46 -0300

    adicionando projeto api-gateway

---
+++ api-gateway/src/main/java/com/github/ianmaciel7/schoolmicroservice/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.github.ianmaciel7.schoolmicroservice.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes().route(p -> p
+				.path("/example/**")
+				.uri("http://localhost:2222/"))
+				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ api-gateway/src/test/java/com/github/ianmaciel7/schoolmicroservice/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ianmaciel7.schoolmicroservice.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
